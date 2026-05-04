1e3c07440494610b078422a0fa51f3785f2a4fc4
GraysonDouglas90 <grayson.douglas@gbscooks.com>
2026-05-04 12:45:22 -0400

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,1 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = 'https://eczrtjcrfzoqiykxxtah.supabase.co'
+const supabaseKey = 'sb_publishable_o3wpq8aJZBs0US-zsteG3g_3_sex6Me'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
+export default supabase
